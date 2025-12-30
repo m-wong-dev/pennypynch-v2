7527c9999f265cf24ea07c6aa57a7ca12def2023
Michael Wong <tsz.wong753@gmail.com>
2025-12-30 10:06:37 -0500

    added redirect

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { plaidClient, sessionOptions } from "@/lib/plaid";
 import { Session } from "@/lib/types/session";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
     const session = await getIronSession<Session>(
@@ -12,12 +13,7 @@ export default async function Dashboard() {
     const access_token = session.accessToken;
 
     if (!access_token) {
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false,
-            },
-        };
+        redirect("/");
     }
 
     const response = await plaidClient.accountsBalanceGet({ access_token });
